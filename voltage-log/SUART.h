8eea9f0e0f7d46e157a58aafd2e3fe8d13f700fb
Fritz Webering <fritz@webering.eu>
2016-09-29 00:06:34 +0200

    Fix header mess in voltage-log

---
+++ voltage-log/SUART.h
@@ -0,0 +1,5 @@
+#define SUART_PORT _SFR_IO_ADDR(PORTB)
+#define SUART_DDR _SFR_IO_ADDR(DDRB)
+#define SUART_PIN _SFR_IO_ADDR(PINB)
+#define SUART_TXD 2
+#define SUART_RXD 0
--- voltage-log/main.c
+++ voltage-log/main.c
@@ -11,16 +11,11 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+#include "SUART.h"
 
 #define LEDPIN 4
 #define ADCPIN 3
 #define PWMPIN 1
-
-#define SUART_PORT _SFR_IO_ADDR(PORTB)
-#define SUART_DDR _SFR_IO_ADDR(DDRB)
-#define SUART_PIN _SFR_IO_ADDR(PINB)
-#define SUART_TXD 2
-#define SUART_RXD 0
 
 // Helper macros turn argument into string literal
 #define STRING(arg) STRING_(arg)
